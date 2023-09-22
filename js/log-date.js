afd29df2b9e663e6c8023e4174955da05db6456d
Otho Mantegazza <otho.mantegazza@gmail.com>
2023-09-22 14:14:35 +0200

    logs to csv

--- js/log-date.js
+++ js/log-date.js
@@ -1,8 +1,26 @@
 console.log("ciao")
 
+function logsToCSV(logs) {
+  const csvString = [
+    [
+      "Fonte",
+      "Istante"
+    ],
+    ...logs.map(item => [
+      item.fonte,
+      item.istante
+    ])
+  ]
+   .map(e => e.join(",")) 
+   .join("\n");
+
+console.log(csvString);
+}
+
 function makeLog(logType) {
   const newLog = {fonte: logType, istante: Date.now()}
   console.log(newLog)
   logs.push(newLog)
+  logsToCSV(logs)
   return newLog
 }
