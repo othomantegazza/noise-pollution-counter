7a4a967d978ebdbfb7bd0caea9dc06474480425d
Otho Mantegazza <otho.mantegazza@gmail.com>
2023-09-30 10:30:15 +0200

    human readable date

--- js/log-date.js
+++ js/log-date.js
@@ -9,14 +9,20 @@ function logsToCSV(logs) {
       item.istante
     ])
   ]
-   .map(e => e.join(",")) 
+   .map(e => e.join(", ")) 
    .join("\n");
    
    return csvString
 }
 
 function makeLog(logType) {
-  const newLog = {fonte: logType, istante: Date.now()}
+  const stamp = new Date
+  const d = `${stamp.getDate()}-${stamp.getMonth()+1}-${stamp.getFullYear()}`
+  const t = `${stamp.getHours()}:${stamp.getMinutes()}:${stamp.getSeconds()}`
+  const newLog = {
+    fonte: logType, 
+    istante: d + ' ' + t
+  }
   logs.push(newLog)
   document.getElementById('logsPre').innerHTML = logsToCSV(logs)
   return newLog
